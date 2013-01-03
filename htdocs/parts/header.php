7a73f73bb4c6944360dd413e09dc33a1478e745c
Markus Tacker <m@coderbyheart.de>
2013-01-03 19:31:56 +0100

    Formular für Mailingliste dazu.

--- htdocs/parts/header.php
+++ htdocs/parts/header.php
@@ -7,12 +7,12 @@
     <meta name="author" content="Gründerhub FrankfurtRheinMain | http://gründerhub.de/">
     <!-- See /humans.txt for more infos -->
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="./build/complete-min.20121118.css" type="text/css">
     <!--
     <link rel="stylesheet" href="./assets/normalize.css" type="text/css">
     <link rel="stylesheet" href="./assets/font-awesome.css" type="text/css">
     <link rel="stylesheet" href="./assets/styles.css" type="text/css">
     -->
+    <link rel="stylesheet" href="./build/complete-min.2013013.css" type="text/css">
 </head>
 <body>
 <div id="main" class="clearfix">
@@ -31,11 +31,22 @@
                 <dd>
                     <a href="https://www.facebook.com/gruenderhub" rel="me"><i class="icon-facebook"></i>
                         Facebook</a><br> <a href="https://twitter.com/gruenderhub"><i class="icon-twitter"></i> Twitter</a><br>
-                    <a href="https://plus.google.com/106785101604023095231/posts" rel="me"><i class="icon-google-plus"></i> Google+ </a><br>
-                    <a href="/impressum"><i class="icon-group"></i> Impressum</a>
+                    <a href="https://plus.google.com/106785101604023095231/posts" rel="me"><i class="icon-google-plus"></i>
+                        Google+ </a><br> <a href="/impressum"><i class="icon-group"></i> Impressum</a>
                 </dd>
             </dl>
         </nav>
+        <hr>
+        <h2>Mailingliste</h2>
+
+        <p>Du kannst Dich hier für unsere <a href="http://groups.google.com/group/gruenderhub?hl=de">Mailingliste</a> registrieren. Wir werden deine E-Mail-Adresse nur für
+            Informationen rund um den Gründerhub verwenden und niemals weitergeben.</p>
+
+        <form action="http://groups.google.com/group/gruenderhub/boxsubscribe">
+            <input type=hidden name="hl" value="de"> <label> <input type="enmail" name="email"> </label>
+            <button type="submit">Beitreten</button>
+        </form>
+
         <hr>
         <div class="warning-box">
             <h2>Hinweis</h2>
